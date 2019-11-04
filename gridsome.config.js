f5961abddd4d9ddf25a2a37b52cabf15080bdc5b
David Couronné <couronne.david@gmail.com>
2019-11-04 11:28:45 +0100

    chore(): add @gridsome/source-filesystem and @gridsome/transformer-remark

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,32 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: [],
+  plugins: [
+    {
+      // Create posts from markdown files
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: 'content/posts/*.md',
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: 'Tag',
+            create: true,
+          },
+        },
+      },
+    },
+  ],
+  transformers: {
+    // Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+
+      ],
+    },
+  },
 }
