f90d82fc5b49ed5c68106a6e808c5f59bf50d5a8
David Couronné <couronne.david@gmail.com>
2019-11-04 10:57:36 +0100

    chore(): Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <div>
+      <v-app-bar>
+        <v-btn
+          icon
+          router
+          exact
+          :to="$url('/')"
+        >
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-toolbar-title>
+          {{ $static.metadata.siteName }}
+        </v-toolbar-title>
+        <v-spacer />
+
+        <v-btn :to="$url('/')">
+          Home
+        </v-btn>
+        <v-btn :to="$url('/about')">
+          About
+        </v-btn>
+      </v-app-bar>
+    </div>
+    <v-content>
+      <slot />
+    </v-content>
+  </v-app>
+</template>
+
+<static-query>
+  query {
+    metadata {
+      siteName
+    }
+  }
+</static-query>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import Vuetify from 'vuetify'
+import './assets/style/index.scss'
+
+import DefaultLayout from '~/layouts/Default.vue'
+
+export default function (Vue, { router, appOptions, head, isClient }) {
+  // Set default layout as a global component
+
+  const opts = { icons: {
+    iconfont: 'mdi',
+  } }// opts includes, vuetify themes, icons, etc.
+  Vue.use(Vuetify)
+
+  appOptions.vuetify = new Vuetify(opts)
+  Vue.component('Layout', DefaultLayout)
+}
