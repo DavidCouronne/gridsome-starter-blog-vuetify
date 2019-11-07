2e3bf660606ee30fa36a191056b517170167a684
David Couronné <couronne.david@gmail.com>
2019-11-07 17:09:21 +0100

    feat(): add screenshot

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,11 +3,7 @@
     <v-container>
       <div class="posts">
         <v-row dense>
-          <post-card
-            v-for="edge in $page.posts.edges"
-            :key="edge.node.id"
-            :post="edge.node"
-          />
+          <post-card v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node" />
         </v-row>
       </div>
     </v-container>
@@ -48,8 +44,49 @@ export default {
     // Author,
     PostCard
   },
-  metaInfo: {
-    title: 'Hello, world!'
+ metaInfo() {
+    return {
+      meta: [
+        { name: 'description', "A blog starter with Gridsome and Vuetify" },
+
+        // Some Open Graph Tags
+        { property: 'og:title', content: this.$static.metadata.siteName },
+        { property: 'og:description', "A blog starter with Gridsome and Vuetify" },
+        { property: 'og:image', 'gridsome-starter-blog-vuetify.jpg' },
+        {
+          property: 'og:url',
+          content: this.$static.metadata.siteUrl
+        },
+
+        // Some Twitter Cards Tags
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: this.$static.metadata.siteName },
+        { name: 'twitter:image', 'gridsome-starter-blog-vuetify.jpg' },
+        { name: 'twitter:description', content: "A blog starter with Gridsome and Vuetify" }
+      ],
+      // Some ld+json tags
+      script: [
+        {
+          type: 'application/ld+json',
+          json: {
+            '@context': 'http://schema.org',
+            '@type': 'BlogPosting',
+            description: "A blog starter with Gridsome and Vuetify",
+            headline: this.$static.metadata.siteName,
+            image: 'gridsome-starter-blog-vuetify.jpg'
+          }
+        }
+      ]
+    }
   }
 }
 </script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteUrl
+  }
+}
+</static-query>
