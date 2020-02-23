2836421fe38d81ee22be074beccbca0052257b08
David Couronné <couronne.david@gmail.com>
2020-02-23 21:05:20 +0100

    chore(): tailwind config

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,6 +3,13 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+const postcssPlugins = [
+  tailwind(),
+]
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
+
 
 module.exports = {
   siteName: 'Gridsome Starter Blog Vuetify',
@@ -94,6 +101,13 @@ module.exports = {
           },
         ],
       ],
+    },
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
     },
   },
 }
