9887b7792086eb4da6fd1cfd394c5c8de4655d15
David Couronné <couronne.david@gmail.com>
2020-02-24 15:00:54 +0100

    fix(): remove language name

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 
 import Vuetify from 'vuetify'
 import './assets/style/index.scss'
-import './assets/style/primjs-languages.js'
+// import './assets/style/primjs-languages.js'
 
 import HeroLayout from '~/layouts/Hero.vue'
 import BlogLayout from '~/layouts/Blog.vue'
