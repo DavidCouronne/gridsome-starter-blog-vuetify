25a57651a033502ecaa991ede7e1ee8138a8d06e
David Couronné <couronne.david@gmail.com>
2020-02-25 12:50:29 +0100

    chore(): remove tailwindcss

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,10 +3,6 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-const tailwind = require('tailwindcss')
-// const purgecss = require('@fullhuman/postcss-purgecss')
-const postcssPlugins = [tailwind()]
-// if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
 
 module.exports = {
   siteName: 'Gridsome Starter Blog Vuetify',
@@ -105,13 +101,6 @@ module.exports = {
           },
         ],
       ],
-    },
-  },
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins,
-      },
     },
   },
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+// Vuetify
 import Vuetify from 'vuetify'
-import './assets/style/index.scss'
-// import './assets/style/primjs-languages.js'
+import { vuetifyConfig } from './vuetify.config.js'
 
+// Custom Styles
+import './assets/style/index.scss'
+
+// Layouts
 import HeroLayout from '~/layouts/Hero.vue'
 import BlogLayout from '~/layouts/Blog.vue'
 import TagLayout from '~/layouts/Tag.vue'
@@ -15,19 +19,18 @@ import BaseThemeToggle from '@/components/base/ThemeToggle.vue'
 import BaseItem from '@/components/base/Item.vue'
 import BaseGroup from '@/components/base/Group.vue'
 
-import { vuetifyConfig } from './vuetify.config.js'
-
 export default function (Vue, { head, appOptions }) {
+  // Headers
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
   })
 
+  // Vuetify
   const vuetifyOpts = vuetifyConfig()
-
   Vue.use(Vuetify)
-
   appOptions.vuetify = new Vuetify(vuetifyOpts)
+
   // Set layout as a global component
   Vue.component('HeroLayout', HeroLayout)
   Vue.component('BlogLayout', BlogLayout)
